36eaaae2b1acc0b79afa1f87fc93180216f4f70f
Lisa Burton <lisaannburton1988@Lisas-MBP.home>
2017-04-11 00:14:41 +0100

    Cleanup

--- terraform/example-webserver.tf
+++ terraform/example-webserver.tf
@@ -4,7 +4,19 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
-resource "digitalocean_droplet" "example_tomcat" {
+resource "digitalocean_droplet" "example_tomcat_0" {
+  image      = "Debian-8-x64"
+  name       = "example-tomcat-0"
+  region     = "lon1"
+  size       = "1GB"
+  ssh_keys   = ["dd:eb:43:d4:66:e0:34:16:8b:e4:ca:68:5c:87:49:97"]
+
+  provisioner "local-exec" {
+    command = "sleep 30 && echo \"[dev]\n${digitalocean_droplet.example_tomcat_0.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory"
+  }
+}
+
+resource "digitalocean_droplet" "example_tomcat_1" {
   image      = "Debian-8-x64"
   name       = "example-tomcat-1"
   region     = "lon1"
@@ -12,6 +24,6 @@ resource "digitalocean_droplet" "example_tomcat" {
   ssh_keys   = ["dd:eb:43:d4:66:e0:34:16:8b:e4:ca:68:5c:87:49:97"]
 
   provisioner "local-exec" {
-    command = "sleep 30 && echo \"[dev]\n${digitalocean_droplet.example_tomcat.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory &&  ansible-playbook -i inventory ../configure-tomcat-server.yml"
+    command = "sleep 30 && echo \"${digitalocean_droplet.example_tomcat_1.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" >> inventory"
   }
 }
