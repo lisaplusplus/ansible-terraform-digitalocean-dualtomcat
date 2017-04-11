f516b72a41259b438e0a59aa92ac51c5cddf4113
Lisa Burton <lisaannburton1988@Lisas-MBP.home>
2017-04-11 21:37:42 +0100

    Using terrform ansible inventort plugin

--- terraform/example-webserver.tf
+++ terraform/example-webserver.tf
@@ -10,10 +10,6 @@ resource "digitalocean_droplet" "example_tomcat_0" {
   region     = "lon1"
   size       = "1GB"
   ssh_keys   = ["dd:eb:43:d4:66:e0:34:16:8b:e4:ca:68:5c:87:49:97"]
-
-  provisioner "local-exec" {
-    command = "sleep 30 && echo \"[dev]\n${digitalocean_droplet.example_tomcat_0.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" > inventory"
-  }
 }
 
 resource "digitalocean_droplet" "example_tomcat_1" {
@@ -22,8 +18,4 @@ resource "digitalocean_droplet" "example_tomcat_1" {
   region     = "lon1"
   size       = "1GB"
   ssh_keys   = ["dd:eb:43:d4:66:e0:34:16:8b:e4:ca:68:5c:87:49:97"]
-
-  provisioner "local-exec" {
-    command = "sleep 30 && echo \"${digitalocean_droplet.example_tomcat_1.ipv4_address} ansible_connection=ssh ansible_ssh_user=root\" >> inventory"
-  }
 }
